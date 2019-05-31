de89a08856863326e0ed40701a35b726e9cb85bf
JangmiTokki <13933554+JangmiTokki@users.noreply.github.com>
2019-05-31 13:05:00 -0600

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,11 @@
 # Build your say_hello method here
+
+#Prints hello with name
+def say_hello(name)
+  puts "Hello #{name}!"
+end
+
+#When no name given, prints default message
+def say_hello(name="Ruby Programmer")
+    puts "Hello #{name}!"
+end
